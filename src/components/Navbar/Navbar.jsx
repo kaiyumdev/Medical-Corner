a5286554e7ff9c9c5892bc4c15722ce225a93d9a
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-03 03:50:48 +0600

    feat(layout): starting medical-corner project finishing navbar & footer component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,6 +11,8 @@ const Navbar = () => {
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
           <li><a>Item 1</a></li>
           <li><a>Item 3</a></li>
+          <li><a>Item 1</a></li>
+          <li><a>Item 3</a></li>
         </ul>
       </div>
       <a className="btn btn-ghost text-xl">daisyUI</a>
