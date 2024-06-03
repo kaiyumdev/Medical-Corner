9200c339ae5d041caf55d3f65d5fe57052e02b12
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-03 06:00:28 +0600

    feat(about): complete about section successfully

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../../pages/HeroSection'
+import About from '../../pages/About'
 
 const Home = () => {
   return (
     <div>
         <HeroSection></HeroSection>
+        <About></About>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from "../../../public/images/Logo_1.png"
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-white my-3">
+        <div className="navbar bg-white my-3 px-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
