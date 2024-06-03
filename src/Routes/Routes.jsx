3fe28d5bd0fbe4a0b08ab4ca4f3ba1ee10d92dfe
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-03 11:59:08 +0600

    feat(routes): Implements router for link the component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/ErrorPage";
 import Home from "../components/Home/Home";
+import Services from "../pages/Services";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Appointment from "../pages/Appointment";
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +16,22 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            },
+            {
+                path: "/services",
+                element: <Services></Services>
+            },
+            {
+                path: "/about",
+                element: <About></About>
+            },
+            {
+                path: "/contact",
+                element: <Contact></Contact>
+            },
+            {
+                path: "/appointment",
+                element: <Appointment></Appointment>
             }
         ]
     }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Logo from "../../../public/images/Logo_1.png"
+// import Logo from "../../../pubLinkc/images/Logo_1.png"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
@@ -7,27 +8,27 @@ const Navbar = () => {
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinknecap="round" strokeLinknejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
-                        <li><a>Home</a></li>
-                        <li><a>Services</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Contact</a></li>
-                        <li><a>Appointment</a></li>
+                        <li> <Link to={"/"}>Home</Link></li>
+                        <li> <Link to={"/services"}>Services</Link></li>
+                        <li><Link to={"/about"}>About</Link></li>
+                        <li><Link to={"/contact"}>Contact</Link> </li>
+                        <li><Link to={"/appointment"}>Appointment</Link></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">
-                    <img className='w-1/2' src={Logo}></img>
-                </a>
+                <Link className="btn btn-ghost text-xl">
+                    <img className='w-1/2' src="/images/Logo_1.png"></img>
+                </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li><a>Services</a></li>
-                    <li><a>About</a></li>
-                    <li><a>Contact</a></li>
-                    <li className='bg-customBlue rounded-lg'><a>Appointment</a></li>
+                <li> <Link to={"/"}>Home</Link></li>
+                        <li> <Link to={"/services"}>Services</Link></li>
+                        <li><Link to={"/about"}>About</Link></li>
+                        <li><Link to={"/contact"}>Contact</Link> </li>
+                        <li><Link className='bg-customBlue rounded-lg' to={"/appointment"}>Appointment</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
