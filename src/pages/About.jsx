e094ba7b788900f1d2328aebf264b3b5e4930a26
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-03 11:36:29 +0600

    (about): updated about component to solve some styles issues

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <section className="pt-24 xs:pt-16 pb-24 xs:pb-16">
-            <div className="container mx-auto">
+            <div className="container mx-auto px-4">
                 <div className="flex flex-wrap justify-center items-center">
                     <div className="w-full xl:w-1/2 sm:w-9/12 lg:w-5/12 md:w-7/12 relative animate-fadeInLeft" style={{ visibility: 'visible', animationDuration: '1s', animationName: 'fadeInLeft' }}>
                         <div className="relative">
@@ -19,22 +19,22 @@ const About = () => {
                         </div>
                     </div>
                     <div className="w-full xl:w-1/2 sm:w-full lg:w-7/12 animate-fadeInRight" style={{ visibility: 'visible', animationDuration: '1s', animationName: 'fadeInRight' }}>
-                        <div>
+                        <div className="px-4 md:px-0">
                             <h5 className="text-primary">About Us</h5>
                             <h2 className="text-3xl font-bold mb-4">The Great Place of Medical Hospital Center.</h2>
                             <p className="mb-6">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum susp endisse ultrices gravida tempor incididu ut labore et dolore magna aliqua. Quis ipsum susp endisse ultrices gravida.
                             </p>
-                        </div>
 
-                        <ul className="flex flex-wrap mb-6">
-                            <li className="mr-4 mb-2">Ambulance Services</li>
-                            <li className="mr-4 mb-2">Oxizen on Wheel</li>
-                            <li className="mr-4 mb-2">Pharmacy on Clinic</li>
-                            <li className="mr-4 mb-2">On duty Doctors</li>
-                            <li className="mr-4 mb-2">24/7 Medical Emergency</li>
-                        </ul>
-                        <a href="about.html" className="btn btn-primary">Discover More</a>
+                            <ul className="flex flex-wrap mb-6">
+                                <li className="mr-4 mb-2">Ambulance Services</li>
+                                <li className="mr-4 mb-2">Oxizen on Wheel</li>
+                                <li className="mr-4 mb-2">Pharmacy on Clinic</li>
+                                <li className="mr-4 mb-2">On duty Doctors</li>
+                                <li className="mr-4 mb-2">24/7 Medical Emergency</li>
+                            </ul>
+                            <a href="about.html" className="btn btn-primary">Discover More</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -43,50 +43,3 @@ const About = () => {
 };
 
 export default About;
-
-
-// import React from 'react';
-
-// const About = () => {
-//     return (
-//         <section className="pt-24 xs:pt-16 pb-24 xs:pb-16">
-//             <div className="container mx-auto">
-//                 <div className="flex flex-wrap justify-center items-center">
-//                     <div className="w-full xl:w-1/2 sm:w-9/12 lg:w-5/12 md:w-7/12 animate-fadeInLeft" style={{ visibility: 'visible', animationDuration: '1s', animationName: 'fadeInLeft' }}>
-//                         <div className="relative">
-//                             <div className="mb-4">
-//                                 <img src="images/about-img1.jpg" alt="about img" className="img-fluid w-full" />
-//                             </div>
-//                             <div className="relative z-10">
-//                                 <img src="images/about_img2.jpg" alt="about img" className="img-fluid w-full" />
-//                                 <a className="absolute inset-0 flex items-center justify-center play_btn venobox vbox-item" data-autoplay="true" data-vbtype="video" href="https://youtu.be/nqye02H_H6I">
-//                                     <i className="fas fa-play text-white" aria-hidden="true"></i>
-//                                 </a>
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div className="w-full xl:w-1/2 sm:w-full lg:w-7/12 animate-fadeInRight" style={{ visibility: 'visible', animationDuration: '1s', animationName: 'fadeInRight' }}>
-//                         <div>
-//                             <h5 className="text-primary">About Us</h5>
-//                             <h2 className="text-3xl font-bold mb-4">The Great Place of Medical Hospital Center.</h2>
-//                             <p className="mb-6">
-//                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum susp endisse ultrices gravida tempor incididu ut labore et dolore magna aliqua. Quis ipsum susp endisse ultrices gravida.
-//                             </p>
-//                         </div>
-
-//                         <ul className="flex flex-wrap mb-6">
-//                             <li className="mr-4 mb-2">Ambulance Services</li>
-//                             <li className="mr-4 mb-2">Oxizen on Wheel</li>
-//                             <li className="mr-4 mb-2">Pharmacy on Clinic</li>
-//                             <li className="mr-4 mb-2">On duty Doctors</li>
-//                             <li className="mr-4 mb-2">24/7 Medical Emergency</li>
-//                         </ul>
-//                         <a href="about.html" className="btn btn-primary">Discover More</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default About;
