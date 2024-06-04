d34182aef59716912eecd1c58691f6faed0ade49
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-04 19:28:09 +0600

    feat(profile-id): remove id because autometically get id from mongoDB & create profile upper dashboard section

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,10 +1,24 @@
 import { Link, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const DashboardLayout = () => {
+  const {user} = useAuth()
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-2 bg-customLightBlue min-h-screen p-12">
         <ul>
+        <li className="border border-black p-4 text w-full flex-col items-center">
+        <div className="avatar">
+                    <div className="w-12 rounded-full border-2 border-black">
+                      <img src={user?.photoURL || "/public/placeholder.jpg"} />
+                    </div>
+                  </div>
+            <Link to={""}>
+              
+
+                  Profile
+            </Link>
+          </li>
           <li className="border border-black p-4 text w-full">
             <Link to={""}>Dashboard</Link>
           </li>
--- src/pages/AddService.jsx
+++ src/pages/AddService.jsx
@@ -9,10 +9,9 @@ const AddService = () => {
         const image = form.image.value;
         const name = form.name.value;
         const description =  form.description.value;
-        const id = form.id.value;
-        const data = {title, name, image, description, id}
+        const data = {title, name, image, description}
 
-        await fetch("http://localhost:3000/services", {
+        await fetch("http://localhost:5000/services", {
             method: "POST",
             headers:{ "Content-type": "application/json"},
             body: JSON.stringify(data)
@@ -27,9 +26,6 @@ const AddService = () => {
         <h1 className='text-5xl font-bold text-center'>Add Sercice</h1>
         <div className='my-16'>
             <form onSubmit={handleSubmit}>
-                <div className='mt-2'>
-                    <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="id" id="" placeholder='id'/>
-                </div>
                 <div className='mt-2'>
                     <input className='bg-gray-100 p-4 w-full border border-black rounded-lg ' type="text" name="title" id="" placeholder='title'/>
                 </div>
