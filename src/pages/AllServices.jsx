7c7def25880fe736aaf6b2c45ba226c9818b3491
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-04 19:34:42 +0600

    feat(allServices): get all services data from server

--- src/pages/AllServices.jsx
+++ src/pages/AllServices.jsx
@@ -5,7 +5,7 @@ const AllServices = () => {
     const [services, setServices] = useState();
 
     useEffect(() =>{
-        fetch("http://localhost:3000/services")
+        fetch("http://localhost:5000/services")
         .then((res) => res.json())
         .then((data) => setServices(data))
     },[])
